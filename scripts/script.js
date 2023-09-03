12478e80fcb66f127a76a6bcb8fee4760a1e3326
Bruno Iorio <brunoig2007@gmail.com>
2023-09-03 15:15:30 -0300

    Pagina inicial: Agregada

--- scripts/script.js
+++ scripts/script.js
@@ -18,6 +18,6 @@ window.onscroll = () => {
   scroll = window.scrollY;
   ancho = document.documentElement.clientWidth;
   scroll > 100 && ancho > 768
-    ? (nav.style.borderBottom = ".2rem var(--color-primario) solid")
-    : (nav.style.borderBottom = "none");
+    ? (nav.style.borderBottom = ".2rem var(--color-primario) solid", nav.style.background = "white")
+    : (nav.style.borderBottom = "none", nav.style.background = "white");
 };
